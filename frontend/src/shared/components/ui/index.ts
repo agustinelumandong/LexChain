03c3a5b42d156dfa98275e55ebbc1ad2fa694720
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2026-04-30 13:50:31 +0800

    refactor(frontend): add feature barrel exports

---
+++ frontend/src/shared/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { BottomNav } from './bottom-nav';
+export { Button } from './button';
+export { Collapsible } from './collapsible';
+export { IconSymbol } from './icon-symbol';
+export { SearchInputWithResults } from './search-input-with-results';
+export { SelectDropdownField } from './select-dropdown-field';
