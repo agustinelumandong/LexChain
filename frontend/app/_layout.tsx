7cd6def9cac0ef6215f71695f0c16f9cdbfd9583
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2026-05-07 14:48:31 +0800

    chore(routes): add document/[id] route

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -45,6 +45,7 @@ export default function RootLayout() {
                 <Stack.Screen name="(auth)" options={{ headerShown: false }} />
                 <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
                 <Stack.Screen name="upload" options={{ headerShown: false }} />
+                <Stack.Screen name="document/[id]" options={{ headerShown: false }} />
                 <Stack.Screen
                   name="camera-capture"
                   options={{ headerShown: false }}
