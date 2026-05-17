d6f70d4ce423e0c48d2611e37366fa66d6369847
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2026-05-17 18:26:36 +0800

    refactor(document): move menu to screens/ and update exports
    - Move document menu to screens/document-menu-screen.tsx
    - Update feature index exports with new components

--- apps/mobile/src/features/document/index.ts
+++ apps/mobile/src/features/document/index.ts
@@ -1,3 +1,5 @@
 export * from './components';
 export * from './services';
 export * from './services/whitelist-storage';
+export { default as DocumentDetailsScreen } from './screens/document-details-screen';
+export { default as DocumentMenuScreen } from './screens/document-menu-screen';
