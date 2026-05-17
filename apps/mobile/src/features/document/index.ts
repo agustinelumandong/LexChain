1fa1d8684ae9ef752a4f63313f1d0b97d1535028
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2026-05-18 02:30:06 +0800

    refactor(document): refactor ask-document-card to use sub-components
    - Refactor ask-document-card to use new components
    - Update feature index exports

--- apps/mobile/src/features/document/index.ts
+++ apps/mobile/src/features/document/index.ts
@@ -1,6 +1,7 @@
 export * from './components';
 export * from './services';
 export * from './services/whitelist-storage';
+export type * from './types/ask-document-card.types';
 export type * from './types/detail-sections.types';
 export { default as DocumentDetailsScreen } from './screens/document-details-screen';
 export { default as DocumentMenuScreen } from './screens/document-menu-screen';
