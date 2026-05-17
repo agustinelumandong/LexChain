860bdebf0198bb50d21dcf1d32703ef3d93459ca
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2026-05-18 02:22:32 +0800

    refactor(document): refactor detail-sections-card to use sub-components
    - Refactor detail-sections-card to use new components
    - Update feature index exports

--- apps/mobile/src/features/document/index.ts
+++ apps/mobile/src/features/document/index.ts
@@ -1,6 +1,7 @@
 export * from './components';
 export * from './services';
 export * from './services/whitelist-storage';
+export type * from './types/detail-sections.types';
 export { default as DocumentDetailsScreen } from './screens/document-details-screen';
 export { default as DocumentMenuScreen } from './screens/document-menu-screen';
 export { default as DocumentPdfViewerScreen } from './screens/document-pdf-viewer-screen';
