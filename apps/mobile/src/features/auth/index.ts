0f3d7c9600733ddc03fb3d5f10bb4740d602ef2a
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2026-05-18 18:38:25 +0800

    refactor(auth): update index exports
    - Update feature index exports with new screens

--- apps/mobile/src/features/auth/index.ts
+++ apps/mobile/src/features/auth/index.ts
@@ -1,6 +1,8 @@
 export { AuthHeader } from './auth-header';
 export { AuthInput } from './auth-input';
 export { AuthScreenShell } from './auth-screen-shell';
+export { default as ForgotPasswordScreen } from './screens/forgot-password-screen';
+export { default as SignInScreen } from './screens/sign-in-screen';
 export { default as SignUpScreen } from './screens/sign-up-screen';
 export { default as TermsBottomSheet } from './terms-bottom-sheet';
 export {
