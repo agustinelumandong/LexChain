112587a16db84e273feeac066f68ea71a6d00223
Sean Agustine Esparagoza <agustinelumandong@gmail.com>
2026-05-18 13:55:25 +0800

    refactor(document): refactor document-actions-sheet and add components
    - Add document-action-item
    - Add document-actions-sheet.styles
    - Refactor document-actions-sheet to use sub-components
    - Update components index exports

--- apps/mobile/src/features/document/components/index.ts
+++ apps/mobile/src/features/document/components/index.ts
@@ -2,6 +2,7 @@ export { AccessWhitelistCard } from './access-whitelist-card';
 export { AskDocumentCard } from './ask-document-card';
 export { AskDocumentSheet } from './ask-document-sheet';
 export { DetailSectionsCard } from './detail-sections-card';
+export { DocumentActionItem } from './document-action-item';
 export { DocumentMenuRow } from './details/document-menu-row';
 export { DocumentActionsSheet } from './document-actions-sheet';
 export { DocumentPreviewBottomSheet } from './document-preview-bottom-sheet';
